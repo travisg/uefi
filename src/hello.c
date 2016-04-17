69f2a959071f384e45b8babe9f634b75598e46bd
Brian Swetland <swetland@frotz.net>
2016-04-17 02:53:01 -0700

    [hello] update to not depend on efilib
    Illustrate the difference between the "raw" efi interface and the
    various helper goodies in -lefi

--- src/hello.c
+++ src/hello.c
@@ -1,13 +1,18 @@
 
 #include <efi.h>
+
+#if USE_EFILIB
 #include <efilib.h>
 
 EFI_STATUS efi_main(EFI_HANDLE img, EFI_SYSTEM_TABLE *sys) {
+	InitializeLib(img, sys);
+	Print(L"Hello, EFI World! (with efilib)\n");
+	return EFI_SUCCESS;
+}
+#else
+EFI_STATUS efi_main(EFI_HANDLE img, EFI_SYSTEM_TABLE *sys) {
         SIMPLE_TEXT_OUTPUT_INTERFACE *ConOut = sys->ConOut;
-
-        InitializeLib(img, sys);
-
-	ConOut->OutputString(ConOut, L"Hello, EFI World!\n");
-
+	ConOut->OutputString(ConOut, L"Hello, EFI World!\r\n");
         return EFI_SUCCESS;
 }
+#endif
